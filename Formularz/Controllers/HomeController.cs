33c997432ad844532ebf4aa1ecd3f12584a9bd0e
Szymon Gorzula <dragoshare@gmail.com>
2024-11-14 21:04:09 +0100

    Dodaj pliki projektów.

---
+++ Formularz/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Formularz.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Formularz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else return View();
+
+        }
+        public IActionResult Wynik(Dane dane)
+        {
+
+            return View(dane);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Formularz/Models/Dane.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formularz.Models
+{
+    public class Dane
+    {
+        [Required(ErrorMessage = "Prosze podaj Imie")]
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Haslo")]
+        public string Haslo { get; set; }
+        [Required(ErrorMessage = "Prosze potwierdz Haslo")]
+        public string Potw_Haslo { get; set; }
+        [Required(ErrorMessage = "Prosze podaj numer telefonu")]
+        public string Nr_telefonu { get; set; }
+        [Required(ErrorMessage = "Prosze podaj swoj wiek")]
+        public int Wiek { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Miasto z ktorego pochodzisz")]
+        public string Miasto { get; set; }
+
+
+    }
+}
